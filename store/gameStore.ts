3110f568e5a3fc3d85a48a1f97abd392fbaf478a
Sergio Marcilla <sermara70@gmail.com>
2025-12-01 18:48:41 +0100

    feat: implement initial competition display and duel page with Zustand state management (#5)

--- store/gameStore.ts
+++ store/gameStore.ts
@@ -1,16 +1,27 @@
 import { create } from 'zustand'
 
 type Player = {
-  id: string
   name: string
+  image: string
+  ranking: number
+}
+
+type Competition = {
+  name: string
+  jornada: number
+  players: Player[]
 }
 
 type GameState = {
+  selectedCompetition: Competition | null
+  setSelectedCompetition: (competition: Competition | null) => void
   currentPlayers: Player[]
   setCurrentPlayers: (players: Player[]) => void
 }
 
 export const useGameStore = create<GameState>((set) => ({
+  selectedCompetition: null,
+  setSelectedCompetition: (competition) => set({ selectedCompetition: competition }),
   currentPlayers: [],
   setCurrentPlayers: (players) => set({ currentPlayers: players }),
 }))
